85f791c6cc37590aff05d53f0c4a74e90ea37105
Alanna Zoscak <amzosc001@gmail.com>
2022-05-07 11:42:49 -0400

    Noise as argument and adjust figues

--- load_and_eval_DCAE.py
+++ load_and_eval_DCAE.py
@@ -16,7 +16,7 @@ def model_load(model_dir):
 
     return model
 
-def data_load(data_dir, resize_shape):
+def data_load(data_dir, noise_level, resize_shape):
     test = []
     image_files = [f for f in os.listdir(data_dir) if f.endswith('.jpg')]
     for image in image_files:
@@ -30,7 +30,7 @@ def data_load(data_dir, resize_shape):
     testX = np.expand_dims(testX, axis=-1)
     testX = testX.astype("float32") / 255.0
 
-    testNoise = np.random.normal(loc=0.0, scale=0.1, size=testX.shape)
+    testNoise = np.random.normal(loc=0.0, scale=noise_level, size=testX.shape)
     testXNoisy = np.clip(testX + testNoise, 0, 1)
     
     return testX, testXNoisy
@@ -39,24 +39,27 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('-d', '--data_dir', default='day', type=str, help='The directory containing the subset of CULane data')
     parser.add_argument('-m', '--model_dir', default='dcae', type=str, help="The directory containing the saved model information")
+    parser.add_argument('-n','--noise_level', default=0.1, type=float, help="The standard deviation of the gaussian noise distribution")
     args = parser.parse_args()
 
     data_dir = args.data_dir
     model_dir = args.model_dir
+    noise_level = args.noise_level
 
     model = model_load(model_dir)
 
     height = 128
     width = 320
     img_shape = (height, width) 
-    test, test_noisy = data_load(data_dir, resize_shape=(width, height))
+    test, test_noisy = data_load(data_dir, noise_level=noise_level, resize_shape=(width, height))
     
     # print(test_noisy.shape)
     test_loss = model.evaluate(test_noisy, test)
     print("Test set loss:", test_loss)
 
     num_display = 3
-    fig, ax = plt.subplots(nrows=3, ncols=num_display, sharey='row')
+    fig, ax = plt.subplots(nrows=3, ncols=num_display, sharey='row', sharex='all', figsize=(10,4.5))
+    fig.suptitle("Noise Level=%.1f, Test Loss=%.5f" % (noise_level, test_loss), fontsize=14)
     for r in range(len(ax)):
         for c in range(len(ax[0])):
             ax[r,c].set_xticks([])      
@@ -64,7 +67,7 @@ if __name__ == "__main__":
 
     y_labels = ["Original", "Noisy", "Reconstructed"]
     for i in range(len(y_labels)):
-        ax[i,0].set_ylabel(y_labels[i])
+        ax[i,0].set_ylabel(y_labels[i], fontsize=14)
 
     for c in range(num_display):
         # Plot original
@@ -81,6 +84,7 @@ if __name__ == "__main__":
         reconstructed = model.predict(noisy_example, batch_size=1)
         ax[2,c].imshow(reconstructed.reshape(img_shape), cmap=plt.cm.gray)
 
-    plt.tight_layout(pad=0.05)
+    fig.tight_layout(pad=0.05)
+    # fig.subplots_adjust(top=0.85)
     plt.savefig("sample_images.png")
 
